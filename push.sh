7dd6ea402ca19a3511473fbe00375b3bbb66beac
Marcos Dione <mdione@grulic.org.ar>
2016-08-12 12:31:42 +0200

    [+] copy digikam's config file, even is that's backed up separately.

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
+cp ~/local/share/config/digikamrc .
+
 rsync --progress --archive --hard-links --update --delete-after \
     --exclude thumbnails-digikam.db \
     "./$1" "192.168.0.42::Pictures/$1"
