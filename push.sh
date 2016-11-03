9316e4f6c987ea9e001b3cdebece1697b9a0bce3
Marcos Dione <mdione@grulic.org.ar>
2016-11-03 14:23:34 +0100

    [*] more verbose pushes.

--- push.sh
+++ push.sh
@@ -1,5 +1,6 @@
 cp ~/local/share/config/digikamrc .
 
 rsync --progress --archive --hard-links --update --delete-after \
+    --no-inc-recursive --itemize-changes \
     --exclude thumbnails-digikam.db \
     "./$1" "mustang::Pictures/$1"
